fd76b284197f722c0b72c6f4ff2ad34ae689eb81
TabulaRasa <lforjust@gmail.com>
2024-08-01 14:55:49 +0300

    Добавил счетчик ошибок и вывод визуализации текущего состояния виселлицы. Мелкие доработки

--- src/main/java/localhost/Game.java
+++ src/main/java/localhost/Game.java
@@ -5,10 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Game {
-    private static int id;
 
     public Game() {
-        id++;
     }
 
     private String choiceOfWord() {
@@ -16,25 +14,26 @@ public class Game {
     }
 
     public void introGame() {
+        System.out.println("Игра Виселица - угадай слово.");
         Scanner scanner = new Scanner(System.in);
         boolean wantPlay = true;
         while (true) {
-            System.out.println("Игра Виселица - угадай слово.\nS - Старт новой игры;\nE - выход");
-            switch (scanner.nextLine()) {
-                case ("S"):
-                case ("s"):{
+            System.out.println("S - Старт новой игры;\nE - выход");
+            switch (scanner.nextLine().toLowerCase()) {
+                case ("s"): {
                     startGame();
                     break;
                 }
-                case ("E"):
-                case ("e"):{
+                case ("e"): {
                     wantPlay = false;
                     break;
                 }
                 default:
                     System.out.println("-----\nВведите правильный символ\n-----\n");
             }
-            if (!wantPlay) {break;}
+            if (!wantPlay) {
+                break;
+            }
         }
     }
 
@@ -43,19 +42,17 @@ public class Game {
         StringBuilder wordMask = createWordMask(selectedWord);
         Scanner scanner = new Scanner(System.in);
         int err = 0;
-        char inputSumbol = '0';
-        List<String> usedWords= new ArrayList<>();
+        char inputSumbol;
+        List<String> usedWords = new ArrayList<>();
 
         System.out.println(selectedWord);
 
         do {
-            System.out.println(wordMask);
             System.out.println("Введите букву:");
 
             inputSumbol = scanner.next().charAt(0);
-            System.out.println(inputSumbol);
 
-            if (selectedWord.toLowerCase().contains( String.valueOf(inputSumbol).toLowerCase() )) {
+            if (selectedWord.toLowerCase().contains(String.valueOf(inputSumbol).toLowerCase())) {
                 System.out.printf("Буква %s есть в загаданном слове!\n", inputSumbol);
                 usedWords.add(String.valueOf(inputSumbol));
                 wordMask = changeWordMask(selectedWord, wordMask, inputSumbol);
@@ -64,21 +61,22 @@ public class Game {
                 err++;
                 usedWords.add(String.valueOf(inputSumbol));
             }
+
+            System.out.println(wordMask);
+
             if (selectedWord.equalsIgnoreCase(String.valueOf(wordMask))) {
                 System.out.println("\n-----\nВы победили!\n-----\n");
                 break;
             }
 
-//            displayingTheStateOfTheGallows(err) {
-//
-//            };
-
+            displayingTheStateOfTheGallows(err);
             System.out.printf("Уже использованные буквы: %s\n", usedWords);
-        }while(err <=5);
+        } while (err < 5);
 
-        if (err >= 5) {
-
+        if (err == 5) {
+            System.out.println("\n-----\nВы проиграли!\n-----\n");
         }
+
     }
 
     private StringBuilder createWordMask(String word) {
@@ -92,9 +90,35 @@ public class Game {
     private StringBuilder changeWordMask(String word, StringBuilder wordMask, char sumbol) {
         for (int i = 0; i < word.length(); i++) {
             if (String.valueOf(word.charAt(i)).equalsIgnoreCase(String.valueOf(sumbol))) {
-                wordMask.setCharAt(i,word.charAt(i));
+                wordMask.setCharAt(i, word.charAt(i));
             }
         }
         return wordMask;
     }
+
+    private void displayingTheStateOfTheGallows(int err) {
+        System.out.printf("Ошибок допущено: %d\nОсталось ошибок: %d\n", err, 5-err);
+            switch (err) {
+                case (1): {
+                    System.out.println("|");
+                    break;
+                }
+                case (2): {
+                    System.out.println("||");
+                    break;
+                }
+                case (3): {
+                    System.out.println("|||");
+                    break;
+                }
+                case (4): {
+                    System.out.println("||||");
+                    break;
+                }
+                case (5): {
+                    System.out.println("|||||");
+                    break;
+                }
+            }
+        }
 }
